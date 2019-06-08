3f3d265d5727e0077818b4d63848904656c3012d
eugenekuprikov <cualcua@gmail.com>
2019-06-08 14:50:35 +0300

    Создаем функцию для отрисовки облака и его тени

--- js/stat.js
+++ js/stat.js
@@ -10,3 +10,16 @@ var TEXT_HEIGHT = 20;
 var BAR_WIDTH = 40;
 var GAP_WELC = 90;
 var barHeight = CLOUD_HEIGHT - GAP - TEXT_HEIGHT - GAP_WELC;
+
+var renderCloud = function (ctx, x, y, x1, y1, y2, x2, x3, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x1, y1);
+  ctx.lineTo(x, y2);
+  ctx.lineTo(x2, y2);
+  ctx.lineTo(x3, y1);
+  ctx.lineTo(x2, y);
+  ctx.closePath();
+  ctx.fill();
+}
