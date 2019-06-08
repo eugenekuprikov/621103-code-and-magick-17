fe99bcca356e4124acbafb0b17741ceb47ed26fe
eugenekuprikov <cualcua@gmail.com>
2019-06-08 14:47:42 +0300

    Создаем константы для замены 'магических цифр' в коде

--- js/stat.js
+++ js/stat.js
@@ -1,1 +1,12 @@
 'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var COL_GAP = 50;
+var TEXT_HEIGHT = 20;
+var BAR_WIDTH = 40;
+var GAP_WELC = 90;
+var barHeight = CLOUD_HEIGHT - GAP - TEXT_HEIGHT - GAP_WELC;
