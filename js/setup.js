cc04a6cd32c0b0b0fd0ea17dc20c6a149bf894ec
eugenekuprikov <cualcua@gmail.com>
2019-06-10 18:17:01 +0300

    Показываем блок .setup-similar (убираем в js-коде класс .hidden)

--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,8 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
+document.querySelector('.setup-similar').classList.remove('hidden');
+
 var ARR_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var ARR_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var ARR_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
