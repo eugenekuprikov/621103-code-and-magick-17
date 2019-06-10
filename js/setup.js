435cb846374fdf925d753a03e0ebafa3e216088c
eugenekuprikov <cualcua@gmail.com>
2019-06-10 18:20:48 +0300

    Находим блок .setup-similar-list для последующей вставки в него DOM-элементов

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,8 @@ userDialog.classList.remove('hidden');
 
 document.querySelector('.setup-similar').classList.remove('hidden');
 
+var similarListElement = document.querySelector('.setup-similar-list');
+
 var ARR_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var ARR_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var ARR_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
