39d952154169b88b8ba836c79299b0f2b31b0a3f
eugenekuprikov <cualcua@gmail.com>
2019-06-10 18:09:06 +0300

    Создаем функцию генерации случайного элемента массива

--- js/setup.js
+++ js/setup.js
@@ -7,3 +7,9 @@ var ARR_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 
 var ARR_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var ARR_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var ARR_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getArrayRandomElement = function (arr) {
+  if (arr && arr.length) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+};
