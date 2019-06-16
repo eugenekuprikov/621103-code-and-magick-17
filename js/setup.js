19024c8ed3628ed49181a05ba7d64d5c77e3f30d
eugenekuprikov <cualcua@gmail.com>
2019-06-16 23:42:37 +0300

    Создаем константы для работы с клавиатуры и массив цветов фаерболлов

--- js/setup.js
+++ js/setup.js
@@ -12,6 +12,9 @@ var ARR_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 
 var ARR_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var ARR_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var ARR_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var getArrayRandomElement = function (arr) {
   if (arr && arr.length) {
