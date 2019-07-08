cb879f05186c357256de23ba66ed70a12a689bd5
eugenekuprikov <cualcua@gmail.com>
2019-07-08 20:52:53 +0300

    В обработчике событий по нажатию запоминаем начальные координаты окна

--- js/dialog.js
+++ js/dialog.js
@@ -2,3 +2,12 @@
 
 var setupDialogElement = document.querySelector('.setup');
 var dialogHandler = setupDialogElement.querySelector('.upload');
+
+dialogHandler.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+});
