01a9f83802c9e283113695b85bab111a8ad619aa
eugenekuprikov <cualcua@gmail.com>
2019-07-08 20:56:39 +0300

    Создаем функцию, которая рассчитывает координаты смещения окна при движении мыши

--- js/dialog.js
+++ js/dialog.js
@@ -10,4 +10,24 @@ dialogHandler.addEventListener('mousedown', function (evt) {
     x: evt.clientX,
     y: evt.clientY
   };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
+    setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+  };
 });
