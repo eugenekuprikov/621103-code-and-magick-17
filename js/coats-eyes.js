fa7c5dfcd190d3bc93b0c33559f80a3168c19e42
eugenekuprikov <cualcua@gmail.com>
2019-07-18 19:55:40 +0300

    Создаем функцию для хранения массивов цветов элементов

---
+++ js/coats-eyes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  var ARR_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var ARR_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+  window.coatsEyes = {
+    ARR_COATS: ARR_COATS,
+    ARR_EYES: ARR_EYES,
+    FIREBALL_COLORS: FIREBALL_COLORS
+  };
+})();
